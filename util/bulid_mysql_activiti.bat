003c333a235ea8fc0d8b05f0e580bf86b0ab0d87
godinzzjk <2721966670@qq.com>
2019-07-30 10:23:24 +0800

    清空

--- util/bulid_mysql_activiti.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-@ECHO     =======================Mybatis自动生成中...=======================
-md src
-java -jar mybatis_mapper.jar -configfile create_mapper_activiti.xml -overwrite
-pause
--- util/bulid_oracle.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-@ECHO     =======================Mybatis自动生成中...=======================
-md src
-java -jar mybatis_mapper.jar -configfile create_mapper_oracle.xml -overwrite
-pause
